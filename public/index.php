75de338be5875ae01fa9b733fd33d1cc51d59098
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-08-22 09:59:31 -0300

    First commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+  use Psr\Http\Message\ResponseInterface as Response;
+  use Psr\Http\Message\ServerRequestInterface as Request;
+  use Slim\Factory\AppFactory;
+
+  require '../vendor/autoload.php';
+
+  $app = AppFactory::create();
+
+  $app->get('/', function (Request $request, Response $response, $args) {
+    view('home');
+    // $response->getBody()->write("Hello, Marlon");
+    return $response;
+  });
+
+  // Run app
+  $app->run();
